33de84c57038a3cfdb7d130370b5f946c5b50df8
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-07 18:31:18 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -9,13 +9,10 @@ require 'pry'
 # }
 
 def get_the_keys(groceries)
-  groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
-
-  groceries.values
-end
-
-def get_the_keys (dairy)
-  groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
-
-groceries.keys
-end
+  groceries = {
+    dairy: ["milk", "yogurt", "cheese"],
+    vegetable: ["carrots", "broccoli", "cucumbers"],
+    meat: ["chicken", "steak", "salmon"],
+    grains: ["rice", "pasta"]
+  }
+end 
